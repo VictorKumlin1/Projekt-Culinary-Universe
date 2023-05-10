5b4ecacd8068e980a032fc3859fde8d73fd13941
VictorKumlin1 <115473338+VictorKumlin1@users.noreply.github.com>
2023-05-10 22:01:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
 // search = document.getElementById("sök-knapp")
+hamurger = document.querySelector(".hamburger");
+hamurger.onclick = function () {
+  navBar = document.querySelector(".nav-bar");
+  navBar.classList.toggle("active");
+};
 
-let searchText = document.getElementById("txtSearch")
+let searchText = document.getElementById("txtSearch");
 
 searchText.onkeydown = async function (event) {
   if (event.key === "Enter") {
-    event.preventDefault()
+    event.preventDefault();
 
-    let searchTerm = searchText.value // Hämtar det som står i sökrutan
-    console.log("Kommer söka efter", searchTerm)
+    let searchTerm = searchText.value; // Hämtar det som står i sökrutan
+    console.log("Kommer söka efter", searchTerm);
 
     // Det här anropas funktionen för att hämta info från ett API.
     // Vi väntar på svaret med await
-    let results = await search(searchTerm)
+    let results = await search(searchTerm);
 
     // Här anropas funktionen som ansvarar för att "rendera" (alltså rita ut) resultatet
-    renderResults(results)
+    renderResults(results);
 
     // TODO: Skriv kod för att tömma sökfältet igen
   }
-}
+};
 
 // Detta är en asynkron funktion som anropar ett API och returnerar svaret som ett JSON-objekt.
 async function search(searchString) {
@@ -30,16 +35,16 @@ async function search(searchString) {
     */
 
   //Här bygger vi upp den URL som vi ska använda i vårat anrop till APIet.
-  let apiKey = "f031cac33f4e4de5b7f94d3d0a2d64f4" //TODO: Lägg in API-nyckeln från Classroom här.
-  var url = `https://api.spoonacular.com/recipes/complexSearch?query=${searchString}&apiKey=${apiKey}`
-  console.log("Den URL vi kommer anropa: ", url)
+  let apiKey = "f031cac33f4e4de5b7f94d3d0a2d64f4"; //TODO: Lägg in API-nyckeln från Classroom här.
+  var url = `https://api.spoonacular.com/recipes/complexSearch?query=${searchString}&apiKey=${apiKey}`;
+  console.log("Den URL vi kommer anropa: ", url);
 
   //Här används URLen för att göra anrop med den inbyggda funktionen fetch()
-  let response = await fetch(url)
+  let response = await fetch(url);
 
   // Detta gör om resultatet från APIet till ett JSON-objekt.
-  let json = await response.json()
-  return json
+  let json = await response.json();
+  return json;
 }
 
 /*
@@ -47,31 +52,30 @@ async function search(searchString) {
   och skriver ut det i en lista i DOMen.
 */
 function renderResults(results) {
-  let resultDiv = document.getElementById("searchresults") //Hämtar ut diven med id="searchresults" för att lägga in resultatet där
+  let resultDiv = document.getElementById("searchresults"); //Hämtar ut diven med id="searchresults" för att lägga in resultatet där
   // Använd console.log() för att skriva ut resultatet till konsollen och titta på det.
   // Det är ofta bra att titta på hur resultatet ser ut för att få en förståelse för
   // hur man kan skriva koden för att använda resultatet.
-  console.log("resultatet: ", results)
+  console.log("resultatet: ", results);
 
   // TODO: Hämta ut attributet av variablen res (res.results) som innehåller listan med resultat
   // och tilldela variablen allObjects det värdet.
-  let allObjects = results.results
+  let allObjects = results.results;
 
   // Den här loopen används för att lägga in något i DOMen för varje objekt (film) i resultatet.
 
-
   for (let index = 0; index < allObjects.length; index++) {
-    const object = allObjects[index]
-    console.log("loopar igenom objekten ", object)
-    let objectDiv = document.createElement("div")
-    objectDiv.setAttribute("class", "object-div")
+    const object = allObjects[index];
+    console.log("loopar igenom objekten ", object);
+    let objectDiv = document.createElement("div");
+    objectDiv.setAttribute("class", "object-div");
     objectDiv.innerHTML = `
     
     <img src = "${object.image}" 
     <h2> ${object.title} </h2>
 
     
-    `
-    output.appendChild(objectDiv)
+    `;
+    output.appendChild(objectDiv);
   }
 }
