95462167763689cc4ff167989c80e2711d73108f
VictorKumlin1 <115473338+VictorKumlin1@users.noreply.github.com>
2023-05-16 23:47:46 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-// search = document.getElementById("sök-knapp")
-hamburger = document.querySelector(".hamburger");
+let hamburger = document.querySelector(".hamburger");
 hamburger.onclick = function () {
-  navBar = document.querySelector(".nav-bar");
+  let navBar = document.querySelector(".nav-bar");
   navBar.classList.toggle("active");
 };
 
@@ -11,85 +10,77 @@ searchText.onkeydown = async function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
 
-    let searchTerm = searchText.value; // Hämtar det som står i sökrutan
+    let searchTerm = searchText.value;
     console.log("Kommer söka efter", searchTerm);
 
-    // Det här anropas funktionen för att hämta info från ett API.
-    // Vi väntar på svaret med await
     let results = await search(searchTerm);
 
-    // Här anropas funktionen som ansvarar för att "rendera" (alltså rita ut) resultatet
     renderResults(results);
-
-    // TODO: Skriv kod för att tömma sökfältet igen
   }
 };
 
-// Detta är en asynkron funktion som anropar ett API och returnerar svaret som ett JSON-objekt.
 async function search(searchString) {
-  // Använd funktionen fetch för att anropa ett API med rätt parametrar.
-  /* 
-    Om ni vill använder ni The Movie Database API.
-    Det finns dokumentation här https://developers.themoviedb.org/3/getting-started/introduction
-    Ni kan i så fall låna min API-nyckel. Den kommer postas i Classroom.
-    */
+  let url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchString}`;
 
-  //Här bygger vi upp den URL som vi ska använda i vårat anrop till APIet.
-  let apiKey = "f031cac33f4e4de5b7f94d3d0a2d64f4"; //TODO: Lägg in API-nyckeln från Classroom här.
-  var url = `https://api.spoonacular.com/recipes/complexSearch?query=${searchString}&apiKey=${apiKey}`;
   console.log("Den URL vi kommer anropa: ", url);
-  // let apiKey = "78e6098494msh5377515b442380fp105906jsn6dc4264ee58c";
-  // let url = `https://api.yummly.com/v1/api/recipes?_app_id=${appId}&_app_key=${apiKey}&q=${searchString}`;
-  // console.log("The URL we will call: ", url);
 
-  //Här används URLen för att göra anrop med den inbyggda funktionen fetch()
   let response = await fetch(url);
 
-  // Detta gör om resultatet från APIet till ett JSON-objekt.
   let json = await response.json();
   return json;
 }
 
-/*
-  Den här funktionen går igenom sökresultatet som är parametern "results"
-  och skriver ut det i en lista i DOMen.
-*/
 function renderResults(results) {
   let resultdiv = document.getElementById("searchresults");
   console.log("resultatet: ", results);
 
-  // if (resultdiv.children.length > 0) {
-  //   resultdiv.innerHTML = "";
-  // }
-
-  let allObjects = results.results;
+  let allObjects = results.meals;
 
   for (let index = 0; index < allObjects.length; index++) {
-    // const object = allObjects[index];
-    // console.log("loopar igenom objekten ", object);
-    // let objectDiv = document.createElement("div");
-    // objectDiv.setAttribute("class", "object-div");
-    // objectDiv.innerHTML = `
-    // <img src = "${object.image} ""
-    // <h2> ${object.title} </h2>
-    // `;
-    // output.appendChild(objectDiv);
     const object = allObjects[index];
 
     let recipeCardHTML = `
-    <div class ="recipe-samling" >
+      <div class="recipe-samling">
+        <div class="recipe-info">
+          <img class="recipe-img" src="${object.strMealThumb}" alt="${object.strMeal}">
+          <div class="recipe-title">
+            <h2>${object.strMeal}</h2>
+          </div>
+          
+          <button class="recipe-btn">Instructions</button>
 
-    <div class = "recipe-info"
 
-    <h2 class= "object-title" > ${object.title} </h2>
-    <img class="object-img" src  = "${object.image}" >
+          <div class="box" style="display: none;">
+            <h3>${object.strMeal}:</h3>
+            <p>${object.strIngredient1}</p>
+            <p>${object.strIngredient2}</p>
+            <p>${object.strIngredient3}</p>
+            <h3>Instructions:</h3>
+            <p>${object.strInstructions}</p>
+            </div>
+       
+          </div>
 
-    </div>
-    </div>
 
-    
+      </div>
     `;
 
     resultdiv.insertAdjacentHTML("beforeend", recipeCardHTML);
   }
+
+  let recipeButtons = document.querySelectorAll(".recipe-btn");
+  recipeButtons.forEach((button) => {
+    button.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      let box = this.parentNode.querySelector(".box");
+      box.style.display = box.style.display === "none" ? "block" : "none";
+
+      // Scroll to the box
+      box.parentNode.scrollIntoView({ behavior: "smooth", block: "start" });
+
+      this.textContent =
+        box.style.display === "none" ? "Instructions" : "Close";
+    });
+  });
 }
