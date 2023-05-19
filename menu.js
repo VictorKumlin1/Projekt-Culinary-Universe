66bec5191765ea0245284037160a711be0737221
VictorKumlin1 <115473338+VictorKumlin1@users.noreply.github.com>
2023-05-19 21:21:57 +0200

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,7 @@
+//Gemensam javascript
+
+let hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function () {
+  let navBar = document.querySelector(".nav-bar");
+  navBar.classList.toggle("active");
+};
